9b8cffa4f53781e126a08b2bfd48c92c8b233aec
Sudipta <ss.dps47@gmail.com>
2021-04-08 00:19:28 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
 const required = (val) => val && val.length;
-const maxLength = (len) => (val) => !(val) || (val.length <=len)
+const maxLength = (len) => (val) => !(val) || (val.length <= len)
 const minLength = (len) => (val) => !(val) || (val.length >= len)
 
 class CommentForm extends React.Component {
@@ -29,8 +29,8 @@ class CommentForm extends React.Component {
     }
     handleCommentSubmit(values) {
         this.toggleCommentModal()
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+
     }
 
     render() {
@@ -48,7 +48,7 @@ class CommentForm extends React.Component {
                                         className="form-control"
                                         validators={{ required }}
                                     >
-                                        <option defaultChecked>5</option>
+                                        <option>5</option>
                                         <option>4</option>
                                         <option>3</option>
                                         <option>2</option>
@@ -66,10 +66,10 @@ class CommentForm extends React.Component {
                             </Row>
                             <Row className="form-group">
 
-                                <Label htmlFor="yourname" md={12}>Your Name</Label>
+                                <Label htmlFor="yourname" md={12}>Author</Label>
                                 <Col md={12}>
 
-                                    <Control.text model=".yourname" id="yourname" name="yourname"
+                                    <Control.text model=".author" id="author" name="author"
                                         className="form-control"
                                         validators={{
                                             required, minLength: minLength(3), maxLength: maxLength(15)
@@ -77,7 +77,7 @@ class CommentForm extends React.Component {
                                     />
                                     <Errors
                                         className="text-danger"
-                                        model=".yourname"
+                                        model=".author"
                                         show="touched"
                                         messages={{
                                             required: 'Required',
@@ -90,7 +90,7 @@ class CommentForm extends React.Component {
                             <Row className="form-group">
                                 <Label htmlFor="message" md={12}>Comment </Label>
                                 <Col md={12}>
-                                    <Control.textarea model=".message" id="message" name="message"
+                                    <Control.textarea model=".comment" id="comment" name="comment"
                                         rows="12"
                                         className="form-control" />
                                 </Col>
@@ -132,7 +132,7 @@ function RenderDish({ dish }) {
         );
 }
 
-function RenderComment({ comments }) {
+function RenderComment({ comments, addComment, dishId }) {
     const comnt = comments.map(commnt => {
         return (
             <div>
@@ -155,7 +155,7 @@ function RenderComment({ comments }) {
             <ul className='list-unstyled'>
                 {comnt}
             </ul>
-            <CommentForm />
+            <CommentForm dishId={dishId} addComment={addComment} />
         </div>
     )
 }
@@ -187,7 +187,10 @@ const DishDetail = (props) => {
                     <RenderDish dish={props.dish} />
                 </div>
                 <div className="col-12 col-md-5 m-1">
-                    <RenderComment comments={props.comments} />
+                    <RenderComment comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id}
+                    />
                 </div>
 
             </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,61 +9,66 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 
 
 const mapStateToProps = state => {
     return {
-        dishes : state.dishes,
+        dishes: state.dishes,
         comments: state.comments,
         promotions: state.promotions,
         leaders: state.leaders
     }
 }
+const mapDispatchToProps = dispatch => ({
+
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+
+});
 
 
-class Main extends React.Component{
-    constructor(props){
-        super(props);
+class Main extends React.Component {
+
+
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId });
+
     }
 
-    onDishSelect(dishId){
-        this.setState({selectedDish:dishId});
-        
-    }
 
-    
 
-    render(){
+    render() {
         const HomePage = () => {
-            return(
+            return (
                 <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
-                promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-                leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
-          }
-        const DishWithId = ({match}) =>{
+        }
+        const DishWithId = ({ match }) => {
             return (
-                <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-                comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
-    
-            );      
+                <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    addComment={this.props.addComment}
+                />
+            );
         };
-        return(
+        return (
             <div>
-                <Header/>
+                <Header />
                 <Switch>
-                    <Route path='/home' component={HomePage}/>
-                    <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes}/> }/>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
-                    <Route path='/aboutus' component={() => <About leaders = {this.props.leaders}/>}/>
+                    <Route path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
-                <Footer/>
+                <Footer />
             </div>
         );
     }
 
 }
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
